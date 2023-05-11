4bfd3445e84297ded86b5c083576d76dfe524a81
sunnlich <michaleva2004zet@gmail.com>
2023-05-11 21:53:45 +0700

    Добавьте файлы проекта.

---
+++ пз_10_4/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace пз_10_4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<zadacha> ActiveNotes;
+        List<zadacha> CompleteNotes;
+
+       // private List<string> SelectedItems { get; set; } = new List<string>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            ActiveNotes = new List<zadacha>() { new zadacha("Добавить еще задачи")};  //создание листа и добавление записи
+            Update();
+        }
+        public void Update()
+        {
+            Current_tasks.ItemsSource = ActiveNotes.ToList();
+            Complete_tasks.ItemsSource = CompleteNotes.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (TextBox_Zadacha.Text == null) return;
+            ActiveNotes.Add(new zadacha(TextBox_Zadacha.Text));
+            Update();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Current_tasks_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var a = ActiveNotes.FindIndex(u => u == Current_tasks.SelectedItem);
+
+            Update();
+        }
+    }
+}
---
+++ пз_10_4/zadacha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace пз_10_4
+{
+    internal class zadacha
+    {
+        public string Name { get; set; }
+        public zadacha(string name)
+        {
+            Name = name;
+        }
+    }
+}
